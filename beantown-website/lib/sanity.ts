75b925eb16597436630d6dcf86f94415235ce947
sundarcodes <sundar82@gmail.com>
2022-12-19 11:47:39 +0530

    chore: set up sanity and .env

---
+++ beantown-website/lib/sanity.ts
@@ -0,0 +1,15 @@
+import { createClient } from 'next-sanity';
+import { config } from './config';
+
+if (!config.projectId) {
+  throw Error('The Project ID is not set. Check your environment variables.');
+}
+
+export const client = createClient(config);
+
+export const previewClient = createClient({
+  ...config,
+  useCdn: false,
+});
+
+export default client;
