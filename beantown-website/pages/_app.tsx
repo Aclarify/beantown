829e7ade6107f471bf4f1a64fab6014a7fa9a3fc
sundarcodes <sundar82@gmail.com>
2023-01-05 23:50:16 +0530

    fix: commented footer import

--- beantown-website/pages/_app.tsx
+++ beantown-website/pages/_app.tsx
@@ -1,22 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Nav from '../components/nav'
-import Footer from '../components/footer'
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
-config.autoAddCss = false
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Nav from '../components/nav';
+// import Footer from '../components/footer';
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }: AppProps) {
-  return(
+  return (
     <>
-        <Nav/>
-        <main>         
-          <Component {...pageProps}/>         
-        </main>
-        {/* <Footer /> */}
-        
+      <Nav />
+      <main>
+        <Component {...pageProps} />
+      </main>
+      {/* <Footer /> */}
     </>
-
-  )
-    
+  );
 }
