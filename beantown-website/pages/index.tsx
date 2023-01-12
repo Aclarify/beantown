c2bcaa4a354238a2ddd62166aeca741dbcc5e675
sundarcodes <sundar82@gmail.com>
2023-01-12 18:36:33 +0530

    chore: added vscode settings and eslint

--- beantown-website/pages/index.tsx
+++ beantown-website/pages/index.tsx
@@ -1,49 +1,49 @@
-import Nav from "../components/organisms/nav";
-import Brands from "../components/templates/home/brands";
-import Faq from "../components/templates/home/faq";
-import Footer from "../components/organisms/footer";
-import { gql } from "@apollo/client";
-import FooterCta from "../components/templates/home/footer-cta-home";
-import Blogs from "../components/templates/home/blogs";
-import React from "react";
-import ServiceAreas from "../components/templates/home/service-areas";
-import Testimonials from "../components/templates/home/testimonials";
-import WhyUs from "../components/templates/home/why-us";
-import Services from "../components/templates/home/services";
-import HomeHero from "../components/templates/home/home-hero";
-import { Home } from "../typing/gql/graphql";
-import pageQuery from "../lib/graphql/pages/get-home.query";
+import Nav from '../components/organisms/nav';
+import Brands from '../components/templates/home/brands';
+import Faq from '../components/templates/home/faq';
+import Footer from '../components/organisms/footer';
+import { gql } from '@apollo/client';
+import FooterCta from '../components/templates/home/footer-cta-home';
+import Blogs from '../components/templates/home/blogs';
+import React from 'react';
+import ServiceAreas from '../components/templates/home/service-areas';
+import Testimonials from '../components/templates/home/testimonials';
+import WhyUs from '../components/templates/home/why-us';
+import Services from '../components/templates/home/services';
+import HomeHero from '../components/templates/home/home-hero';
+import { Home } from '../typing/gql/graphql';
+import pageQuery from '../lib/graphql/pages/get-home.query';
 import WithGlobalContent, {
-  generateGetStaticProps,
-} from "../components/containers/global-content/global-content.container";
-import { PageNames } from "../configs/client/pages/pages.config";
+	generateGetStaticProps,
+} from '../components/containers/global-content/global-content.container';
+import { PageNames } from '../configs/client/pages/pages.config';
 
 export interface HomePageContentProps {
-  allHome: Home[];
+	allHome: Home[];
 }
 
 const getStaticProps = generateGetStaticProps<HomePageContentProps>(
-  pageQuery,
-  PageNames.HOME
+	pageQuery,
+	PageNames.HOME
 );
 export { getStaticProps };
 
 const HomePage: React.FC = () => {
-  return (
-    <>
-      <Nav />
-      <HomeHero />
-      <Services />
-      <WhyUs />
-      <Testimonials />
-      <ServiceAreas />
-      <Blogs />
-      <Faq></Faq>
-      <Brands />
-      <FooterCta />
-      <Footer />
-    </>
-  );
+	return (
+		<>
+			<Nav />
+			<HomeHero />
+			<Services />
+			<WhyUs />
+			<Testimonials />
+			<ServiceAreas />
+			<Blogs />
+			<Faq></Faq>
+			<Brands />
+			<FooterCta />
+			<Footer />
+		</>
+	);
 };
 
 export default WithGlobalContent<HomePageContentProps>(HomePage);
