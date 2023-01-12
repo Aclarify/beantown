6a5d5a7a01d099b105e59fee9288e72fe53fa4dc
swineethamurthy <swineethamurthy@gmail.com>
2023-01-12 11:58:54 +0530

    feat: moved gql queries to separate page

--- beantown-website/pages/index.tsx
+++ beantown-website/pages/index.tsx
@@ -1,47 +1,35 @@
-import groq from 'groq';
-import client, { graphQLClient } from '../lib/sanity';
-import Nav from '../components/organisms/nav';
-import Brands from '../components/templates/home/Brands';
-import Faq from '../components/templates/home/Faq';
-import Footer from '../components/organisms/Footer';
-import { gql } from '@apollo/client';
-import FooterCta from '../components/templates/home/FooterCta';
-import Blogs from '../components/templates/home/Blogs';
-import React from 'react';
-import ServiceAreas from '../components/templates/home/ServiceAreas';
-import Testimonials from '../components/templates/home/Testimonials';
-import WhyUs from '../components/templates/home/WhyUs';
-import Services from '../components/templates/home/Services';
-import HomeHero from '../components/templates/home/HomeHero';
-import GET_ABOUT_US from '../lib/graphql/pages/get-about-us.query';
-import { RootQuery } from '../typing/gql/graphql';
-
-const GET_HEADER_INFO = gql`
-  query {
-    allAboutUs {
-      name
-    }
-  }
-`;
+import groq from "groq";
+import client, { graphQLClient } from "../lib/sanity";
+import Nav from "../components/organisms/nav";
+import Brands from "../components/templates/home/Brands";
+import Faq from "../components/templates/home/Faq";
+import Footer from "../components/organisms/Footer";
+import { gql } from "@apollo/client";
+import FooterCta from "../components/templates/home/FooterCta";
+import Blogs from "../components/templates/home/Blogs";
+import React from "react";
+import ServiceAreas from "../components/templates/home/ServiceAreas";
+import Testimonials from "../components/templates/home/Testimonials";
+import WhyUs from "../components/templates/home/WhyUs";
+import Services from "../components/templates/home/Services";
+import HomeHero from "../components/templates/home/HomeHero";
+import GET_HOME_QUERY from "../lib/graphql/pages/get-home.query";
+import { RootQuery } from "../typing/gql/graphql";
 
 export async function getStaticProps(context: any) {
   const { data } = await graphQLClient.query({
-    query: GET_ABOUT_US,
+    query: GET_HOME_QUERY,
   });
 
   return {
     props: {
-      aboutUsInfo: data.allAboutUs,
+      homeInfo: data.allHome,
     },
   };
 }
 
-export default function Home({
-  aboutUsInfo,
-}: {
-  aboutUsInfo: RootQuery['allAboutUs'];
-}) {
-  console.log('aboutUsInfo', aboutUsInfo);
+export default function Home({ homeInfo }: { homeInfo: RootQuery["allHome"] }) {
+  console.log("homeInfo", homeInfo);
 
   return (
     <>
