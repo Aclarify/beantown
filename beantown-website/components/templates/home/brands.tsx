2520c40eb25b191b750e805acc68d70c92f64285
swineethamurthy <swineethamurthy@gmail.com>
2023-01-22 14:46:19 +0530

    fix: not show empty logos from sanity

--- beantown-website/components/templates/home/brands.tsx
+++ beantown-website/components/templates/home/brands.tsx
@@ -15,12 +15,15 @@ export default function Brands() {
 	return (
 		<section id="brand">
 			<div className="flex  justify-center">
-				<span className="text-xl md:text-2xl font-bold p-4 m-4">{brandsTitle}</span>
+				<span className="text-xl md:text-2xl font-bold p-4 m-4">
+					{brandsTitle}
+				</span>
 			</div>
 			<div className="group flex flex-nowrap overflow-x-auto  md:space-x-2 p-4 m-4">
 				{clientsLogo?.map((logo, index) => {
 					return (
-						logo && (
+						logo &&
+						logo.image && (
 							<div
 								key={index}
 								className=" group-hover:scale-[0.55] hover:!scale-100 duration-500 flex w-[100px] h-[100px] md:w-[250px] md:h-[250] md:p-4"
