566dab04038f2ac91a802d450cbb9d5a33516a64
swineethamurthy <swineethamurthy@gmail.com>
2023-01-26 14:17:43 +0530

    fix : fixed service card image sizing

--- beantown-website/components/templates/home/services/service-card.tsx
+++ beantown-website/components/templates/home/services/service-card.tsx
@@ -13,37 +13,30 @@ interface Props {
 
 const ServiceCard: React.FC<Props> = (props) => {
 	const imagePath = props.thumbnailSrc;
-	console.log('inside service card', imagePath);
 	return (
-		<>
-			<div className="flex">
-				<div className="rounded-lg shadow-lg flex flex-col justify-center items-center  bg-white max-w-sm ">
-					<div
-						className="w-96 h-72 rounded-t-lg }} "
-						style={{ backgroundImage: `url(${imagePath})` }}
-					></div>
-					{/* <Image
-							src={props.thumbnailSrc}
-							alt={props.thumbnailAltText}
-							width="400"
-							height="350"
-							className='rounded-t-lg h-72 object-cover'
-						
-						/> */}
-
-					<div className="flex flex-col justify-center items-center">
-						<h1 className="p-4  title-5 md:title-3 text-primary-shade-1 text-center">
-							{props.title}
-						</h1>
-						<Link href={'/'}>
-							<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
-								{props.buttonText}
-							</Button>
-						</Link>
-					</div>
-				</div>
+		<div id="card-container" className="flex-col bg-white rounded-lg shadow-lg">
+			<div id="image-container" className="basis-3/4">
+				<Image
+					src={props.thumbnailSrc}
+					alt={props.thumbnailAltText}
+					width="400"
+					height="350"
+					className="rounded-t-lg h-72 object-cover"
+				/>
 			</div>
-		</>
+			<div id="name-container" className="basis-1/8">
+				<h1 className="p-4  title-5 md:title-3 text-primary-shade-1 text-center">
+					{props.title}
+				</h1>
+			</div>
+			<div id="button-container" className="basis-1/8 text-center">
+				<Link href={'/'}>
+					<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
+						{props.buttonText}
+					</Button>
+				</Link>
+			</div>
+		</div>
 	);
 };
 export default ServiceCard;
