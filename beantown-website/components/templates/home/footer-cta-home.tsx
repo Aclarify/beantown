37fe39d77e6368a768ad7a01b031d160cf6c9ea1
swineethamurthy <swineethamurthy@gmail.com>
2023-01-27 23:56:26 +0530

    fix: footer cta height issue

--- beantown-website/components/templates/home/footer-cta-home.tsx
+++ beantown-website/components/templates/home/footer-cta-home.tsx
@@ -5,6 +5,7 @@ import { HomePageContentProps } from 'pages';
 import RichText from 'components/molecules/rich-text.molecule';
 import Button from 'components/atoms/button.atom';
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function FooterCta() {
 	const { pageContent } =
@@ -23,16 +24,17 @@ export default function FooterCta() {
 						id="content"
 						className=" bg-gradient-to-r from-secondary-shade-1  to-secondary-shade-2 relative  basis-1/2 "
 					>
-						<img
+						<Image
+							src="/images/home_page/footer_cta_theme/mask_group.svg"
 							className="object-fill animate-pulse object-center "
-							src="images/home_page/footer_cta_theme/mask_group.svg"
+							alt="Image mask"
+							height={600}
+							width={600}
 						/>
-						<div className=" flex-col items-center justify-center absolute inset-x-0 inset-y-4 px-10   ">
-							<div>
-								<p className="title-5 md:title-2 text-primary-black text-left">
-									{ctaTitle}
-								</p>
-							</div>
+						<div className="flex flex-col md:text-left justify-center absolute inset-x-0 inset-y-4 px-10   ">
+							<p className="title-5 md:title-2 text-primary-black text-left">
+								{ctaTitle}
+							</p>
 							<div className="para-2 text-primary-shade-1 justify-start w-3/4 ">
 								<RichText value={ctaDescription?.contentRaw} />
 							</div>
@@ -46,7 +48,7 @@ export default function FooterCta() {
 						</div>
 					</div>
 					<div
-						className={`basis-1/2 height-auto`}
+						className={`basis-1/2 h-[600px]`}
 						style={{
 							backgroundImage: `linear-gradient(
 							to right,
