4a7a1ecca14806f9d3b48bc46545ff05be6d57ba
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-05 19:36:39 +0530

    feat:blob bg image

--- beantown-website/components/templates/home/why-us/why-us.tsx
+++ beantown-website/components/templates/home/why-us/why-us.tsx
@@ -27,35 +27,46 @@ export default function WhyUs() {
 				maskSize: 'cover',
 			}}
 		>
-			<div className="flex flex-col-reverse md:flex md:flex-row  items-center w-full mt-20  ">
-				<div className=" lg:pt-20  flex flex-col items-start text-justify basis-1/2 ">
-					<h1 className="title-5 md:title-2 text-primary-black ">
-						{whyUsTitle}
-					</h1>
-					<div className="md:para-2 para-4 text-primary-shade-1 text-left lg:py-10">
-						<RichText value={whyUsDescription?.contentRaw} />
-					</div>
+			<div className="flex flex-col-reverse  md:flex md:flex-row  items-center w-full md:mt-20 ">
+				<div className=" md:mt-20 flex flex-col items-start text-justify basis-1/2 ">
+					<div className="">
+						<h1 className="title-5 md:title-2 text-primary-black ">
+							{whyUsTitle}
+						</h1>
+						<div className="md:para-2 para-4 text-primary-shade-1 text-left lg:py-10">
+							<RichText value={whyUsDescription?.contentRaw} />
+						</div>
 
-					<Link href={'/'}>
-						<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
-							{whyUsButton?.text}
-						</Button>
-					</Link>
+						<Link href={'/'}>
+							<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
+								{whyUsButton?.text}
+							</Button>
+						</Link>
+					</div>
 				</div>
-				<div className="lg:mt-12 px-20 basis-1/2">
-					<Image
-						src={whyUsImage?.asset?.url || ''}
-						alt={whyUsImage?.asset?.altText || ''}
-						width="1000"
-						height="1000"
-						style={{
-							WebkitMaskImage: "url('./why-us-mask.svg')",
-							WebkitMaskSize: 'cover',
-							maskImage: "url('./why-us-mask.svg')",
-							maskSize: 'cover',
-						}}
-						className="aspect-w-16 aspect-h-9 mt-24"
-					/>
+				<div className=" px-20 md:mt-20 basis-1/2  ">
+					<div className="aspect-w-16 aspect-h-9  relative flex items-center  ">
+						<Image
+							src={'/whyus-bg-shape.svg'}
+							alt={'Background  vector Shapes'}
+							width="1000"
+							height="700"
+							className="aspect-w-16 aspect-h-9  absolute "
+						/>
+						<Image
+							src={whyUsImage?.asset?.url || ''}
+							alt={whyUsImage?.asset?.altText || ''}
+							width="1000"
+							height="700"
+							style={{
+								WebkitMaskImage: "url('./why-us-mask.svg')",
+								WebkitMaskSize: 'cover',
+								maskImage: "url('./why-us-mask.svg')",
+								maskSize: 'cover',
+							}}
+							className="aspect-w-16 aspect-h-9  absolute "
+						/>
+					</div>
 				</div>
 			</div>
 
