845ea7d5be40cdcdb06116493be140ff9091905b
sundarcodes <sundar82@gmail.com>
2023-02-07 22:47:58 +0530

    fix: removed margin issue in why us

--- beantown-website/components/templates/home/why-us/why-us.tsx
+++ beantown-website/components/templates/home/why-us/why-us.tsx
@@ -16,10 +16,6 @@ export default function WhyUs() {
 	const homeData = pageContent.home[0];
 	const { whyUsTitle, whyUsDescription, whyUsButton, whyUsCards, whyUsImage } =
 		homeData;
-
-
-	
-
 	return (
 		<section
 			id="whyUs"
@@ -31,52 +27,52 @@ export default function WhyUs() {
 				maskSize: 'cover',
 			}}
 		>
-			<div className=" mt-96  flex flex-col-reverse   sm:flex sm:flex-row  items-center w-full md:mt-20 ">
-				<div className=" md:mt-10 flex flex-col items-start text-justify basis-1/2  ">
-					<div className="">
-						<h1 className="title-5 md:title-2 text-primary-black ">
-							{whyUsTitle}
-						</h1>
-						<div className="md:para-2 para-4 text-primary-shade-1 text-left lg:py-10">
-							<RichText value={whyUsDescription?.contentRaw} />
-						</div>
+			<div className="flex flex-col-reverse lg:flex lg:flex-row  items-center w-full mt-20  ">
+				<div className=" lg:pt-20  flex flex-col items-start text-justify basis-1/2 ">
+					<h1 className="title-5 lg:title-2 text-primary-black ">
+						{whyUsTitle}
+					</h1>
+					<div className="lg:para-2 para-4 text-primary-shade-1 text-left lg:py-10">
+						<RichText value={whyUsDescription?.contentRaw} />
+					</div>
 
-						<Link href={'/'}>
-							<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
-								{whyUsButton?.text}
-							</Button>
-						</Link>
-					</div>
+					<Link href={'/'}>
+						<Button fontColor="text-white" bgColor=" bg-primary-shade-1">
+							{whyUsButton?.text}
+						</Button>
+					</Link>
 				</div>
-				<div className="md:px-20 py-20  md:mt-10 basis-1/2 mt-20  ">
-					<div className=" relative flex items-center  ">
-						<div className="aspect-w-16 aspect-h-9 absolute ">
-							<Image
-								src={'/whyus-bg-shape.svg'}
-								alt={'Background  vector Shapes'}
-								width="1000"
-								height="700"
-							/>
-						</div>
-						<div className="aspect-w-16 aspect-h-9  absolute ">
-							<Image
-								src={whyUsImage?.asset?.url || ''}
-								alt={whyUsImage?.asset?.altText || ''}
-								width="1000"
-								height="700"
-								style={{
-									WebkitMaskImage: "url('./why-us-mask.svg')",
-									WebkitMaskSize: 'cover',
-									maskImage: "url('./why-us-mask.svg')",
-									maskSize: 'cover',
-								}}
-							/>
-						</div>
-					</div>
+				<div className="mt-12 px-20 basis-1/2 hidden lg:block">
+					<Image
+						src={whyUsImage?.asset?.url || ''}
+						alt={whyUsImage?.asset?.altText || ''}
+						width="1000"
+						height="1000"
+						style={{
+							WebkitMaskImage: "url('./why-us-mask.svg')",
+							WebkitMaskSize: 'cover',
+							maskImage: "url('./why-us-mask.svg')",
+							maskSize: 'cover',
+						}}
+					/>
+				</div>
+				<div className="block lg:hidden md:mt-10">
+					<Image
+						src={whyUsImage?.asset?.url || ''}
+						alt={whyUsImage?.asset?.altText || ''}
+						width="500"
+						height="500"
+						style={{
+							WebkitMaskImage: "url('./why-us-mask.svg')",
+							WebkitMaskSize: 'cover',
+							maskImage: "url('./why-us-mask.svg')",
+							maskSize: 'cover',
+						}}
+					/>
 				</div>
 			</div>
 
-			<div className="py-6 grid grid-cols-1 md:grid-cols-2 gap-2    lg:mt-20">
+			<div className="py-6 grid grid-cols-1 lg:grid-cols-2 gap-2    lg:mt-20">
 				{whyUsCards?.map((ourValues, index) => {
 					return (
 						ourValues?.image && (
