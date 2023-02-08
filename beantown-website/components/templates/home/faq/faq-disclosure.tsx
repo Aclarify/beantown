14d2d453453befb765191d9fbd583aa6be2d9f76
swineethamurthy <swineethamurthy@gmail.com>
2023-02-08 17:33:59 +0530

    fix : FAQ component fixes

--- beantown-website/components/templates/home/faq/faq-disclosure.tsx
+++ beantown-website/components/templates/home/faq/faq-disclosure.tsx
@@ -9,19 +9,19 @@ interface Props {
 
 const FAQDisclosure: React.FC<Props> = (props) => {
 	return (
-		<div className="py-2 rounded-lg">
+		<div className="py-2 min-w-[80%]">
 			<Disclosure defaultOpen={props.isOpen}>
 				{({ open, close }) => (
 					<>
 						<div
 							className={`${
 								open && props.isOpen
-									? 'bg-white'
+									? 'bg-white shadow-lg shadow-gray-shade-4 rounded-lg'
 									: 'bg-transparent border rounded-lg'
 							} `}
 						>
 							<Disclosure.Button
-								className="flex w-full justify-between rounded-lg  px-4 py-2 text-left subtitle-4 lg:subtitle-2 text-primary-shade-1 hover:bg-black-200 focus:outline-none focus-visible:ring focus-visible:ring-black-500 focus-visible:ring-opacity-75"
+								className="flex w-full justify-between p-4 text-left "
 								onClick={() => {
 									if (open) {
 										close();
@@ -29,11 +29,21 @@ const FAQDisclosure: React.FC<Props> = (props) => {
 									props.handleClick();
 								}}
 							>
-								<span>{props.faq}</span>
+								<span
+									className={`${
+										open && props.isOpen
+											? 'subtitle-6 lg:subtitle-4 text-primary-shade-1'
+											: 'subtitle-7 lg:subtitle-4 text-gray-shade-1'
+									} `}
+								>
+									{props.faq}
+								</span>
 								<ChevronDownIcon
 									className={`${
-										open && props.isOpen ? 'rotate-180 transform' : ''
-									} h-5 w-5 text-black-500`}
+										open && props.isOpen
+											? 'rotate-180 transform'
+											: 'text-gray-shade-1'
+									} lg:h-8 lg:w-8 h-6 w-6 text-black-500`}
 								/>
 							</Disclosure.Button>
 							<Transition
@@ -46,7 +56,7 @@ const FAQDisclosure: React.FC<Props> = (props) => {
 								leaveTo="opacity-0 scale-95"
 							></Transition>
 							{props.isOpen && (
-								<Disclosure.Panel className="p-4 para-4 lg:para-3 text-primary-shade-1 bg-white">
+								<Disclosure.Panel className="p-4 para-4 lg:para-3 text-primary-shade-1 bg-white rounded-b-lg">
 									<div className="border-t-2 pt-2">
 										<span>{props.faqAnswer} </span>
 									</div>
