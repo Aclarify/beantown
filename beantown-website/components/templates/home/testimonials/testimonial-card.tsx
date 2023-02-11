36fe9f53d645da0c299b653ac484821c72fc2560
sundarcodes <sundar82@gmail.com>
2023-02-11 07:06:34 +0530

    fix: scroll y in testimonial card

--- beantown-website/components/templates/home/testimonials/testimonial-card.tsx
+++ beantown-website/components/templates/home/testimonials/testimonial-card.tsx
@@ -12,7 +12,7 @@ const TestimonialCard: React.FC<Props> = (props) => {
 	return (
 		<section id="testimonialCard">
 			<div className="">
-				<div className="flex justify-center mt-4">
+				<div className="mt-4 flex justify-center">
 					<Image
 						alt="An image for quotes in testimonial card"
 						width={40}
@@ -20,14 +20,14 @@ const TestimonialCard: React.FC<Props> = (props) => {
 						src="/images/home/testimonialCardsImageUnionVector.svg"
 					/>
 				</div>
-				<div className="flex flex-col gap-2 items-center pt-10 pb-6">
+				<div className="flex flex-col items-center gap-2 pt-10 pb-6">
 					<h1 className="subtitle-2 md:subtitle-4 text-primary-black">
 						{props.clientName}
 					</h1>
 					<h4 className="md:para-3 para-4 text-gray-shade-2">
 						{props.clientDetails}
 					</h4>
-					<div className="pt-10 para-4 md:para-3 text-center text-primary-black  overflow-y-scroll ">
+					<div className="para-4 md:para-3 text-primary-black pt-6  text-center ">
 						<ReadMore
 							content={props.reviewComments}
 							limit={250}
