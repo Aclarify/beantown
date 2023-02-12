11af170f8ae47c83518b1800d945c6ac759c923e
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-12 23:23:19 +0530

    feat:slicker-carousel-implemented

--- beantown-website/components/templates/home/testimonials/testimonials.tsx
+++ beantown-website/components/templates/home/testimonials/testimonials.tsx
@@ -8,25 +8,15 @@ import { HomePageContentProps } from 'pages';
 import Image from 'next/image';
 import TestimonialCard from './testimonial-card';
 import TestimonialModal from './testimonial-modal';
+import Head from 'next/head';
+import Slider from 'react-slick';
+
 
 export default function Testimonials() {
 	const [showTestimonialModel, setShowTestimonialModel] = useState(false);
 	const [selectedTestimonial, setSelectedTestimonial] = useState<any>(null);
-	const [currentIndex, setCurrentIndex] = useState(0);
-	const maxScrollWidth = useRef(0);
-	const carousel = useRef<any>(null);
 
-	useEffect(() => {
-		maxScrollWidth.current = carousel.current
-			? carousel.current.scrollWidth - carousel.current.offsetWidth
-			: 0;
-	}, []);
-	useEffect(() => {
-		if (carousel !== null && carousel.current !== null) {
-			carousel.current.scrollLeft = carousel.current.offsetWidth * currentIndex;
-		}
-	}, [currentIndex]);
-
+	const slider = useRef();
 	const { pageContent } =
 		useContext<GlobalContextProps<HomePageContentProps>>(GlobalContext);
 
@@ -43,38 +33,7 @@ export default function Testimonials() {
 		document.body.style.overflow = 'unset';
 	};
 
-	const movePrevious = () => {
-		if (currentIndex > 0) {
-			setCurrentIndex((previousState) => previousState - 1);
-		}
-	};
-	const moveNext = () => {
-		if (
-			carousel.current !== null &&
-			carousel.current.offsetWidth * currentIndex <= maxScrollWidth.current
-		) {
-			setCurrentIndex((previousState) => previousState + 1);
-		}
-	};
-
-	const isDisabled = (direction: any) => {
-		if (direction === 'prev') {
-			return currentIndex <= 0;
-		}
-
-		if (direction === 'next' && carousel.current !== null) {
-			return (
-				carousel.current.offsetWidth * currentIndex >= maxScrollWidth.current
-			);
-		}
-
-		return false;
-	};
-
-	const goToSlide = (slideIndex: number) => {
-		setCurrentIndex(slideIndex);
-	};
-
+	
 	const onTestimonialCardClick = (testimonial: any) => {
 		setSelectedTestimonial(testimonial);
 		setShowTestimonialModel(true);
@@ -83,10 +42,65 @@ export default function Testimonials() {
 			document.body.style.overflow = 'hidden';
 		}
 	};
+  
+	const next = () => {
+	slider.current.slickNext();
+	};
+	const previous = () => {
+	slider.current.slickPrev();
+	}; 
 
+	var settings = {
+		infinite: true,
+		speed: 500,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		initialSlide: 0,
+		arrows: false,
+		dots: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+					initialSlide: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+		],
+	};
 	return (
 		<>
-			<section
+			<Head>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					charSet="UTF-8"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+				/>
+				<link
+					rel="stylesheet"
+					type="text/css"
+					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+				/>
+			</Head>
+
+			<main
 				id="testimonials-cards"
 				className=" bg-secondary-shade-3  overflow-hidden lg:px-10"
 			>
@@ -136,68 +150,19 @@ export default function Testimonials() {
 					</div>
 				</div>
 
-				<div className="m-4 flex flex-nowrap space-x-4  overflow-x-auto ">
-					<div className="gap-15 z-10 m-8 hidden flex-none flex-col items-center   justify-center lg:flex  ">
-						<div className="flex-none  gap-5 ">
-							<h1 className=" text-light-1 text-primary-shade-1 ">
-								{testimonialTitle}
-							</h1>
-							<span className=" title-2 text-primary-black">
-								{testimonialDescription}
-							</span>
-						</div>
-						<div className="m-4 hidden items-center justify-center lg:flex lg:flex-col ">
-							<div className="flex  justify-center pt-4 ">
-								<button
-									onClick={movePrevious}
-									disabled={isDisabled('prev')}
-									className="bg-primary-shade-1 z-10 m-1 h-full w-24 rounded-full  p-2 text-center text-white transition-all duration-300 ease-in-out hover:opacity-100  disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
-									aria-label="Button for moving left"
-								>
-									<FontAwesomeIcon icon={faArrowLeft} size="lg" />
-								</button>
-								<button
-									onClick={moveNext}
-									disabled={isDisabled('next')}
-									className="bg-primary-shade-1 z-10 m-1 h-full  w-24 rounded-full p-2   text-center text-white transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
-									aria-label="Button for moving right"
-								>
-									<FontAwesomeIcon icon={faArrowRight} size="lg" />
-								</button>
-							</div>
-						</div>
+				<div className="gap-15 z-10 m-8 hidden flex-none flex-col items-center   justify-center lg:flex  ">
+					<div className="flex-none  gap-5 ">
+						<h1 className=" text-light-1 text-primary-shade-1 ">
+							{testimonialTitle}
+						</h1>
+						<span className=" title-2 text-primary-black">
+							{testimonialDescription}
+						</span>
 					</div>
-
-					<div
-						ref={carousel}
-						className="no-scrollbar group z-0 mt-16    flex snap-x snap-mandatory gap-6  overflow-x-scroll scroll-smooth "
-					>
-						{testimonialCards?.map((reviews, index) => {
-							return (
-								<div
-									// id="slider"
-									key={index}
-									className=" my-6 h-[70vh] max-h-[500px] w-80 flex-none snap-start items-center rounded-2xl border  bg-[#FFFFFF] p-2 "
-								>
-									<TestimonialCard
-										key={index}
-										clientName={reviews?.titleText || ''}
-										clientDetails={reviews?.subText || ''}
-										reviewComments={reviews?.description || ''}
-										onShowMore={() => onTestimonialCardClick(reviews)}
-									/>
-								</div>
-							);
-						})}
-					</div>
-				</div>
-
-				<div className="lg:hidden">
-					<div className="mr-8 flex items-center justify-end ">
-						<div className="flex justify-center pt-4 ">
+					<div className="m-4 hidden items-center justify-center lg:flex lg:flex-col ">
+						<div className="flex  justify-center pt-4 ">
 							<button
-								onClick={movePrevious}
-								disabled={isDisabled('prev')}
+								onClick={next}
 								className="bg-primary-shade-1  z-10 m-1 h-full w-16  rounded-full  p-2   text-center text-white transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
 								aria-label="Button for moving left"
 							>
@@ -205,8 +170,7 @@ export default function Testimonials() {
 							</button>
 							<button
 								id="right"
-								onClick={moveNext}
-								disabled={isDisabled('next')}
+								onClick={previous}
 								className="bg-primary-shade-1 z-10 m-1 h-full  w-16 rounded-full   p-2  text-center text-white transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
 								aria-label="Button for moving right"
 							>
@@ -215,6 +179,50 @@ export default function Testimonials() {
 						</div>
 					</div>
 				</div>
+				<div className="">
+					<div className="relative">
+						<Slider ref={(current) => (slider.current = current)} {...settings}>
+							{testimonialCards?.map((reviews, index) => {
+								return (
+								
+									<div
+										key={index}
+										className=" my-6 h-[70vh] max-h-[500px] w-80 flex-none snap-start items-center rounded-2xl border  bg-[#FFFFFF] p-2  "
+									>
+										<TestimonialCard
+											key={index}
+											clientName={reviews?.titleText || ''}
+											clientDetails={reviews?.subText || ''}
+											reviewComments={reviews?.description || ''}
+											onShowMore={() => onTestimonialCardClick(reviews)}
+										/>
+									</div>
+								
+									
+								);
+							})}
+						</Slider>
+					</div>
+				</div>
+
+				<div className="flex items-center justify-center pt-10  lg:hidden">
+					<button
+						onClick={next}
+						className="bg-primary-shade-1  z-10 m-1 h-full w-16  rounded-full  p-2   text-center text-white transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
+						aria-label="Button for moving left"
+					>
+						<FontAwesomeIcon icon={faArrowLeft} />
+					</button>
+					<button
+						id="right"
+						onClick={previous}
+						className="bg-primary-shade-1 z-10 m-1 h-full  w-16 rounded-full   p-2  text-center text-white transition-all duration-300 ease-in-out hover:opacity-100 disabled:cursor-not-allowed disabled:bg-white disabled:text-black"
+						aria-label="Button for moving right"
+					>
+						<FontAwesomeIcon icon={faArrowRight} />
+					</button>
+				</div>
+
 				<TestimonialModal
 					onClose={handleOnClose}
 					visible={showTestimonialModel}
@@ -238,7 +246,7 @@ export default function Testimonials() {
 					/>
 				/>
 				</Modal> */}
-			</section>
+			</main>
 		</>
 	);
 }
