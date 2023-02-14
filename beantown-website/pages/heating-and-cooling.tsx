4e0d522d21c8a547d738cea525c9431397c5867f
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-14 12:36:45 +0530

    HC Page import removed

--- beantown-website/pages/heating-and-cooling.tsx
+++ beantown-website/pages/heating-and-cooling.tsx
@@ -1,29 +1,15 @@
 import React from 'react';
-import Image from 'next/image';
-import Nav from '../components/organisms/nav';
-import Brands from '../components/templates/home/brands';
-import Faq from '../components/templates/home/faq/faq.section';
-import Footer from '../components/organisms/footer';
-import Blogs from '../components/templates/home/blogs/blogs';
-import HeatingCoolingPageHero from '../components/templates/heating-and-cooling/heating-cooling-hero';
-import MassSave from '../components/templates/heating-and-cooling/heating-cooling-mass-save';
-import FooterCtaHeatingCooling from '../components/templates/heating-and-cooling/heating-cooling-footer-cta';
 import HeatingCoolingServiceCard from '../components/templates/heating-and-cooling/heating-cooling-service-cards';
 import HeatingCoolingProductsCards from '../components/templates/heating-and-cooling/heating-cooling-product-cards';
+import Nav from 'components/organisms/nav';
 
 export default function CoolingandHeating() {
 	return (
 		<>
-			<Nav />
-			<HeatingCoolingPageHero />
+			
 			<HeatingCoolingServiceCard />
 			<HeatingCoolingProductsCards />
-			<MassSave />
-			<FooterCtaHeatingCooling />
-			<Brands />
-			<Blogs />
-			<Faq />
-			<Footer />
+			
 		</>
 	);
 }
