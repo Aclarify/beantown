024593682e41c7995ecec138b84ad866a2401c4d
swineethamurthy <swineethamurthy@gmail.com>
2023-02-16 00:04:34 +0530

    fix : services styling changes

--- beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
+++ beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
@@ -12,21 +12,17 @@ interface Props {
 const HeatingCoolingServiceCard: React.FC<Props> = (props) => {
 	return (
 		<>
-			<div className=" m-8 flex   flex-col justify-center  space-y-2 p-8  shadow-[rgba(29,_39,_87,_0.04)_0px_4px_8px]  sm:h-[448px] sm:w-[354px] ">
-				<div className="flex justify-center sm:h-80 sm:w-80">
-					<Image
-						src={props.thumbnailSrc}
-						alt={props.thumbnailAltText}
-						width={80}
-						height={80}
-					></Image>
-				</div>
-				<div className="sm:w-148 text-center  sm:h-60 ">
-					<p className="subtitle-1">{props.title}</p>
-				</div>
-
-				<div className="text-center sm:h-[180px]  sm:w-[284px]">
-					<p>{props.content}</p>
+			<div className="z-10 m-8 mx-auto flex flex-col place-content-center justify-center space-y-6  rounded-3xl bg-[#FFFFFF]  p-8 shadow-[rgba(29,_39,_87,_0.04)_0px_6px_10px]  sm:m-12 sm:p-12 ">
+				<Image
+					src={props.thumbnailSrc}
+					alt={props.thumbnailAltText}
+					width={100}
+					height={100}
+					className="mx-auto"
+				></Image>
+				<div className="flex flex-col  space-y-6 text-center">
+					<p className="title-5 md:title-3">{props.title}</p>
+					<p className="para-2 md:para-1 ">{props.content}</p>
 				</div>
 			</div>
 		</>
