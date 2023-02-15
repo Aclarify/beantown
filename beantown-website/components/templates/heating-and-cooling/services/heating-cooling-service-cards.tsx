11f905e32c4b5e58acb5f42b7c31474edd5a86c3
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-15 22:35:51 +0530

    feat:z-index added to the blob

--- beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
+++ beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
@@ -12,7 +12,7 @@ interface Props {
 const HeatingCoolingServiceCard: React.FC<Props> = (props) => {
 	return (
 		<>
-			<div className="m-8 mx-auto flex h-[448px] w-[354px] flex-col place-content-center justify-center space-y-6  rounded-lg bg-[#FFFFFF]  p-8 shadow-[rgba(29,_39,_87,_0.04)_0px_4px_8px]  sm:m-12 sm:p-12 ">
+			<div className="z-10 m-8 mx-auto flex h-[448px] w-[354px] flex-col place-content-center justify-center space-y-6  rounded-lg bg-[#FFFFFF]  p-8 shadow-[rgba(29,_39,_87,_0.04)_0px_4px_8px]  sm:m-12 sm:p-12 ">
 				<Image
 					src={props.thumbnailSrc}
 					alt={props.thumbnailAltText}
--- beantown-website/components/templates/heating-and-cooling/services/heating-cooling-services.tsx
+++ beantown-website/components/templates/heating-and-cooling/services/heating-cooling-services.tsx
@@ -6,6 +6,7 @@ import HeatingCoolingServiceCard from './heating-cooling-service-cards';
 import { GlobalContext } from '@contexts/global/global.context';
 import { GlobalContextProps } from '@typing/common/interfaces/contexts.interface';
 import { HeatingCoolingContentProps } from 'pages/heating-and-cooling';
+import WaveWrapper from 'components/molecules/wave-wrapper.molecule';
 
 export default function HeatingCoolingServices() {
 	const { pageContent } =
@@ -17,57 +18,30 @@ export default function HeatingCoolingServices() {
 
 const { servicesTitle, servicesDescription, servicesGroup } = pageData;
 	return (
-		<section id="heatingCard">
-			<div className="container relative mx-auto w-full">
-				<div className="absolute  -left-1/4 -top-40  h-[500px] w-[500px]">
-					<Image
-						id="hc-services-blob"
-						src={'/images/heating-cooling/services/hc-services-blob.svg'}
-						alt={'blob vector shape Image'}
-						width={500}
-						height={500}
-					></Image>
-				</div>
-				<div className="mt-20 flex flex-col items-center text-center  ">
-					<ContentWrapper>
-						<ContentWrapper.Title>
-							<h1 className="title-2 lg:title-1 text-primary-black mb-4">
-								{servicesTitle}
-							</h1>
-						</ContentWrapper.Title>
-						<ContentWrapper.Description className="para-2 lg:para-1">
-							<RichText value={servicesDescription?.contentRaw} />
-						</ContentWrapper.Description>
-					</ContentWrapper>
-				</div>
-				<div className="container mx-auto ">
-					<div className="grid grid-cols-1  items-center gap-6 rounded-lg bg-[FFFFFF]   md:grid-cols-2 lg:grid-cols-4 ">
-						{servicesGroup?.map((content, index) => {
-							return (
-								<HeatingCoolingServiceCard
-									key={index}
-									title={content?.titleText || ''}
-									content={content?.description || ''}
-									thumbnailSrc={content?.image?.asset?.url || ''}
-									thumbnailAltText={''}
-								/>
-							);
-						})}
-					</div>
-				</div>
-				<div className="absolute -right-1/4 bottom-0 h-[500px] w-[500px]">
-					<Image
-						id="hc-services-blob"
-						src={'/images/heating-cooling/services/hc-services-blob.svg'}
-						alt={'blob vector shape Image'}
-						width={500}
-						height={500}
-					></Image>
-				</div>
 		<section
 			id="heatingServices"
 			className="z-2 relative -mt-[1em] md:-mt-[2em] lg:-mt-[5em] "
 		>
+			<div className="absolute -left-36 top-20 ">
+				<Image
+					id="hc-services-blob"
+					src={'/images/heating-cooling/services/hc-services-blob.svg'}
+					alt={'blob vector shape Image'}
+					width={500}
+					height={500}
+					className="h-auto w-full"
+				></Image>
+			</div>
+			<div className="absolute -right-24 -bottom-36 ">
+				<Image
+					id="hc-services-blob"
+					src={'/images/heating-cooling/services/hc-services-blob.svg'}
+					alt={'blob vector shape Image'}
+					width={500}
+					height={500}
+					className="h-auto w-full"
+				></Image>
+			</div>
 			<WaveWrapper waveURL="/images/heating-cooling/services/hc-services-top-wave.svg"></WaveWrapper>
 			<div className="  flex flex-col items-center text-center ">
 				<ContentWrapper>
@@ -81,7 +55,7 @@ const { servicesTitle, servicesDescription, servicesGroup } = pageData;
 					</ContentWrapper.Description>
 				</ContentWrapper>
 			</div>
-			<div className="  grid grid-cols-1  items-center rounded-lg bg-[FFFFFF]  sm:grid-cols-4 ">
+			<div className="z-10  grid grid-cols-1  items-center rounded-lg bg-[FFFFFF]  sm:grid-cols-4 ">
 				{servicesGroup?.map((content, index) => {
 					return (
 						<HeatingCoolingServiceCard
