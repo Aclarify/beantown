df464551cb62f7c4056dd0b301cdfe8a97f022a8
sundarcodes <sundar82@gmail.com>
2023-02-17 22:44:02 +0530

    chore: data import from dev to prod

---
+++ sanity-studio/scripts/move-dev-to-prod.sh
@@ -0,0 +1,9 @@
+echo "In shell script"
+
+# Export develop DB
+echo "Exporting develop DB"
+# Output to file with current date
+sanity dataset export develop ./exports/develop-$(date +%Y-%m-%d).tar.gz
+# Import the file to the production dataset
+echo "Import develop DB to production"
+sanity dataset import ./exports/develop-$(date +%Y-%m-%d).tar.gz production 
