7216fcce1da9c76ec96cd93453b6eff49598fa82
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-20 11:58:34 +0530

    feat:electrical-services-list-added

---
+++ beantown-website/pages/electrical.tsx
@@ -0,0 +1,11 @@
+import ElectricalServicesList from "components/templates/electrical/electrical-services-list";
+
+
+const ElectricalServices: React.FC = (props) => {
+	return (
+		<main className="">
+			<ElectricalServicesList />
+		</main>
+	);
+}
+    export default ElectricalServices;
