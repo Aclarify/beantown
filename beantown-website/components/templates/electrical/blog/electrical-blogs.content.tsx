0539e3fc4c3fa97dd7a3e6366892650e2a70c4d5
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-23 20:24:24 +0530

    feat:blog data

--- beantown-website/components/templates/electrical/blog/electrical-blogs.content.tsx
+++ beantown-website/components/templates/electrical/blog/electrical-blogs.content.tsx
@@ -1,7 +1,6 @@
 import React, { useContext } from 'react';
 import { GlobalContext } from '@contexts/global/global.context';
 import { GlobalContextProps } from '@typing/common/interfaces/contexts.interface';
-import { HomePageContentProps } from 'pages';
 import Blogs from 'components/organisms/blogs.organism';
 import { ElectricalContentProps } from 'pages/electrical';
 
@@ -13,12 +12,13 @@ const ElectricalBlogsContent = () => {
 	}
 	const homeData = pageContent.page[0];
 	const { blogTitle, blogDescription, blogButton, blogCards } = homeData;
+	console.log('array of data'+ blogCards)
 	return (
 		<Blogs
 			blogsTitle={blogTitle || ''}
 			blogsDescription={blogDescription || ''}
 			blogsButtonText={blogButton?.text || ''}
-			blogsButtonTextColour="text-black"
+			blogsButtonTextColour="text-white"
 			blogsButtonBgColour="bg-service-yellow"
 			blogCards={blogCards || []}
 		></Blogs>
--- beantown-website/components/templates/electrical/blog/electrical-blogs.section.tsx
+++ beantown-website/components/templates/electrical/blog/electrical-blogs.section.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import WaveWrapper from 'components/molecules/wave-wrapper.molecule';
 import SectionContentWrapper from 'components/molecules/section-content-wrapper.molecule';
-import HeatingCoolingBlogsContent from './electrical-blogs.content';
+import ElectricalBlogsContent from './electrical-blogs.content';
 
 const ElectricalBlogsSection = () => {
 	return (
 		<section
-			id="electrical-blogs"			
+			id="electrical-blogs"
 			className=" lg:mb[2em] 3xl:mb-[3em] 
 				relative z-10 -mt-[2em]  mb-[3em] sm:-mt-[3em] md:-mt-[3em] md:mb-[2em] lg:-mt-[3em] lg:mb-[2em] xl:mb-[3em] 2xl:mb-[5em] 2xl:-mt-[4em]"
 		>
 			<WaveWrapper waveURL="/images/electrical/blogs/electrical-blogs-top-wave.svg"></WaveWrapper>
 			<div className="bg-service-yellow-bg">
 				<SectionContentWrapper>
-					<HeatingCoolingBlogsContent />
+					<ElectricalBlogsContent  />
 				</SectionContentWrapper>
 			</div>
 			<WaveWrapper waveURL="/images/electrical/blogs/electrical-blogs-bottom-wave.svg"></WaveWrapper>
