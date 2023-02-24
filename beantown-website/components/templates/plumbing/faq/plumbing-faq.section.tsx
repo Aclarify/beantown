ac408a910f2f4e613e11430d7eb05592e3ac8746
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-24 19:03:51 +0530

    feat:faq margin top reduced 16 to 6

--- beantown-website/components/templates/plumbing/faq/plumbing-faq.section.tsx
+++ beantown-website/components/templates/plumbing/faq/plumbing-faq.section.tsx
@@ -6,7 +6,7 @@ const PlumbingFaqSection = () => {
 		<>
 			<section
 				id="faq"
-				className=" z-5 sm:mt[-7em] 3xl:mt-[-16em] relative mb-[9em] mt-[-6em] pt-[60px] lg:mt-[-8em] lg:pt-0 2xl:mt-[-12em] "
+				className=" z-5 sm:mt[-7em] 3xl:mt-[-6em] relative mb-[9em] mt-[-6em] pt-[60px] lg:mt-[-8em] lg:pt-0 2xl:mt-[-12em] "
 			>
 				<PlumbingFaqContent />
 			</section>
