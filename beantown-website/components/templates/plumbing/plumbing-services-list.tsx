36cf0a556961dd23452db097c96eb48ff029664e
swineethamurthy <swineethamurthy@gmail.com>
2023-02-25 22:03:42 +0530

    fix : plumbing design fixes

--- beantown-website/components/templates/plumbing/plumbing-services-list.tsx
+++ beantown-website/components/templates/plumbing/plumbing-services-list.tsx
@@ -18,7 +18,7 @@ const PlumbingServicesList = () => {
 			id="plumbing-services-list"
 			className=" mt-[-2em] mb-[5em]  h-auto w-full rounded-3xl bg-white shadow-[rgba(29,_39,_87,_0.08)_4px_8px_60px] sm:mt-[0em]"
 		>
-			<div className="container px-2 py-4  lg:p-[44px]  ">
+			<div className="container p-4  lg:p-[44px]  ">
 				<p className="title-6 md:title-4 text-primary-black pb-4  sm:pb-8">
 					{servicesListTitle}
 				</p>
@@ -26,7 +26,11 @@ const PlumbingServicesList = () => {
 					<div className="grid  grid-cols-2   md:grid-cols-4">
 						{servicesList?.map((listItem, index) => {
 							return (
-								<div id="list-items" className="flex gap-2 " key={index}>
+								<div
+									id="list-items"
+									className="flex items-center gap-2 py-1 "
+									key={index}
+								>
 									<Image
 										src={'/images/plumbing/plumbing-services-list-icon.svg'}
 										alt={'plumbing-services-list-icon-image'}
