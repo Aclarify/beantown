636d945a240a59e83a4c8311bb463970d1432cb5
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-02-25 18:21:38 +0530

    fix:hero section description padding added

--- beantown-website/components/templates/plumbing/hero/plumbing-hero.content.tsx
+++ beantown-website/components/templates/plumbing/hero/plumbing-hero.content.tsx
@@ -28,16 +28,16 @@ const PlumbingHeroContent = () => {
 			gradientFromColor="from-service-green"
 			gradientToColor="bg-service-green"
 		>
-			<SectionContentWrapper>
-				<ContentWrapper className="pt-[2em] pb-[4em] text-center md:py-[12em]  lg:py-[6em] lg:text-left">
-					<ContentWrapper.Title className="mb-[8px] lg:mb-[16px]">
+			<SectionContentWrapper >
+				<ContentWrapper className="pt-[4em] pb-[4em] text-center md:py-[12em]  lg:py-[10em] lg:text-left">
+					<ContentWrapper.Title className="mb-[8px] lg:mb-[16px] ">
 						<h1 className="title-4 lg:title-1 text-white">{heroTitle}</h1>
 					</ContentWrapper.Title>
 					<ContentWrapper.Description className="text-white">
 						<RichText value={heroDescription?.contentRaw} />
 					</ContentWrapper.Description>
-					<ContentWrapper.CTA className="mt-[32px] pb-[2em] lg:pb-0">
-						<CtaWrapper.CTA className="bg-primary-shade-1 para-3 lg:para-2 h-[52px] w-[184px] rounded-lg py-1 px-4 tracking-wide text-white  md:py-2 md:px-8 lg:h-[64px] lg:w-[182px] lg:tracking-wider ">
+					<ContentWrapper.CTA className="mt-[28px] pb-[2em] lg:pb-10">
+						<CtaWrapper.CTA className="bg-primary-shade-1 para-3 lg:para-2 h-[52px] w-[184px] rounded-lg py-1 px-4 tracking-wide text-white  md:py-2 md:px-8 lg:h-[64px] lg:w-[182px] lg:tracking-wider">
 							<p>{heroButton?.text}</p>
 						</CtaWrapper.CTA>
 					</ContentWrapper.CTA>
