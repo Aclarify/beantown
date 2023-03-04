344e3528cc65a92fee3ff1cb5cc556e06f846e7b
sundarcodes <sundar82@gmail.com>
2023-03-05 01:01:22 +0530

    fix: fixed footer cta wave margin

--- beantown-website/components/templates/home/footer-cta-home.tsx
+++ beantown-website/components/templates/home/footer-cta-home.tsx
@@ -20,7 +20,7 @@ export default function FooterCta() {
 			<section
 				id="home-cta"
 				className="z-15 3xl:-top-[10em] 3xl:-mb-[4em] relative -top-[1em] mb-[6em] sm:-top-[2em] sm:mb-[5em] md:-top-[3em]
-				md:mb-[5em] lg:-top-[3em] lg:mb-[3em] xl:-top-[4em] 2xl:-top-[6em] 2xl:mb-[0em]"
+				md:mb-[5em] lg:-top-[4em] lg:mb-[3em] xl:-top-[5em] 2xl:-top-[7em] 2xl:mb-[0em]"
 			>
 				<CTAWithImage
 					heroImageURL={ctaImage?.asset?.url || ''}
