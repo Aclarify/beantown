4a17234f9f71c512f834402563059edd09994b17
swineethamurthy <swineethamurthy@gmail.com>
2023-03-05 01:40:21 +0530

    fix: meet our team layout

--- beantown-website/components/templates/about-us/team/meet-our-team.content.tsx
+++ beantown-website/components/templates/about-us/team/meet-our-team.content.tsx
@@ -21,13 +21,13 @@ const MeetOurTeam = () => {
 			<div id="section-container">
 				<div id="title-container" className="flex flex-col">
 					<ContentWrapper>
-						<ContentWrapper.Title className=" mb-3 lg:mb-4">
+						<ContentWrapper.Title className=" mb-3 lg:mb-6">
 							<h1 className="title-5 lg:title-2 text-primary-shade-1 mt-10 text-center">
 								{teamTitle}
 							</h1>
 						</ContentWrapper.Title>
 						<ContentWrapper.Description>
-							<p className="para-4 lg:subtitle-4 text-primary-shade-1 my-4 justify-center text-center md:mx-40 ">
+							<p className="para-4 lg:subtitle-4 text-primary-shade-1 mb-8 justify-center text-center md:mx-40 lg:mb-14">
 								{teamDescription}
 							</p>
 						</ContentWrapper.Description>
@@ -35,7 +35,7 @@ const MeetOurTeam = () => {
 				</div>
 				<div
 					id="cards-container"
-					className=" grid grid-cols-2 gap-4 lg:grid-cols-3 "
+					className=" grid grid-cols-2 justify-items-center gap-x-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-2 xl:grid-cols-3"
 				>
 					{memberCards?.map((content, index) => {
 						return (
@@ -43,42 +43,29 @@ const MeetOurTeam = () => {
 								<div
 									key={index}
 									id="card"
-									className="flex flex-col   rounded-lg  bg-white p-1 shadow-[rgba(29,_39,_87,_0.08)_4px_8px_60px] sm:flex sm:flex-row  sm:p-2 "
+									className="m-2 w-[160px] rounded-lg bg-white  px-3 shadow-[rgba(29,_39,_87,_0.08)_4px_8px_60px]  lg:w-auto lg:px-4 "
 								>
-									{/* <div className="flex h-full items-center">
+									<div
+										id="profileInfo-wrapper"
+										className="flex  flex-col  lg:flex-row  "
+									>
+										<div className="rounded-lg fill-current py-3 lg:w-[200px]  lg:py-4 ">
 											<CMSImageWrapper
 												altText={content.profilePicture?.asset?.altText || ''}
 												image={content.profilePicture || null}
-											/>
-										</div> */}
-
-									<div
-										id="profileInfo-wrapper"
-										className="justify-between sm:flex sm:space-x-5 "
-									>
-										<div className="h-[150px] w-[150px]   rounded-lg fill-current md:h-[200px] md:w-[200px] ">
-											<Image
-												src={content.profilePicture?.asset?.url || ''}
-												height={'200'}
-												width={'200'}
-												alt={content.profilePicture?.asset?.altText || ''}
 											/>
 										</div>
 
 										<div
 											id="profile-Info"
-											className=" mb-4 flex flex-col items-center  justify-between  "
+											className="  flex flex-col items-center justify-center pb-2 lg:items-start lg:justify-evenly lg:py-8 lg:pl-8"
 										>
-											<div id="name" className="flex flex-col ">
-												<h1 className=" lg:title-4  mt-4 text-center text-black sm:mt-10">
-													{content?.memberName}
-												</h1>
-											</div>
-											<div id="Designation">
-												<span className=" text-primary-color-shade-2">
-													{content?.memberDesignation}
-												</span>
-											</div>
+											<h1 className="title-6 lg:title-3   text-primary-black">
+												{content?.memberName}
+											</h1>
+											<span className="subtitle-5 md:subtitle-4 text-gray-shade-1 block !font-thin">
+												{content?.memberDesignation}
+											</span>
 											<div className="flex">
 												{content.socialMediaDetails?.map((details, index) => {
 													return (
@@ -102,7 +89,7 @@ const MeetOurTeam = () => {
 						);
 					})}
 				</div>
-				<div className="items-center pt-4 text-center lg:mb-16">
+				<div className="mt-8 items-center text-center lg:mt-[60px] ">
 					<CtaWrapper.CTA className="para-3 lg:para-2 bg-primary-shade-1 h-[52px] w-[184px] rounded-lg py-1 px-4 tracking-wide text-white  md:py-2  md:px-8 lg:h-[64px] lg:w-[210px] lg:tracking-wider ">
 						<p>{'Load More'}</p>
 					</CtaWrapper.CTA>
