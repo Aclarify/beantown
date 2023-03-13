998eccda6a98fa86c0162cff81cf87aa384733aa
swineethamurthy <swineethamurthy@gmail.com>
2023-03-13 16:18:37 +0530

    fix: static import of about us page waves

--- beantown-website/components/templates/about-us/testimonials/about-us-testimonials.tsx
+++ beantown-website/components/templates/about-us/testimonials/about-us-testimonials.tsx
@@ -4,6 +4,8 @@ import { GlobalContextProps } from '@typing/common/interfaces/contexts.interface
 import { AboutUsContentProps } from 'pages/about-us';
 import Testimonials from 'components/organisms/testimonials.organism';
 import WaveWrapper from 'components/molecules/wave-wrapper.molecule';
+import topWave from 'public/images/about-us/testimonials/about-us-testimonial-top-wave.svg';
+import bottomWave from 'public/images/about-us/testimonials/about-us-testimonial-bottom-wave.svg';
 
 export default function AboutUsTestimonials() {
 	const { pageContent } =
@@ -16,11 +18,7 @@ export default function AboutUsTestimonials() {
 
 	return (
 		<section id="testimonialSection" className="z-5 relative">
-			<WaveWrapper
-				waveURL={
-					'/images/about-us/testimonials/about-us-testimonial-top-wave.svg'
-				}
-			></WaveWrapper>
+			<WaveWrapper waveURL={topWave}></WaveWrapper>
 			<div className=" bg-secondary-shade-3 3xl:mt-[-25em] xs:mt-[-2em] mt-[-4em] md:mt-[-10em] lg:mt-[-12em] xl:mt-[-15em] 2xl:mt-[-20em]">
 				<Testimonials
 					testimonialTitle={testimonialSection?.testimonialTitle || ''}
@@ -30,11 +28,7 @@ export default function AboutUsTestimonials() {
 					testimonialCards={testimonialSection?.testimonialCards || []}
 				/>
 			</div>
-			<WaveWrapper
-				waveURL={
-					'/images/about-us/testimonials/about-us-testimonial-bottom-wave.svg'
-				}
-			></WaveWrapper>
+			<WaveWrapper waveURL={bottomWave}></WaveWrapper>
 		</section>
 	);
 }
