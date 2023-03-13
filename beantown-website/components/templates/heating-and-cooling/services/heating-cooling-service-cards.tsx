5f9e2b064051f52602b3c006f5194fdbee4b8784
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-03-13 16:45:56 +0530

    feat:heating and cooling animation added

--- beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
+++ beantown-website/components/templates/heating-and-cooling/services/heating-cooling-service-cards.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
+import Fade from 'react-reveal/Fade';
 interface Props {
 	title: string;
 	thumbnailSrc: string;
@@ -11,27 +12,30 @@ const HeatingCoolingServiceCard: React.FC<Props> = (props) => {
 	return (
 		<>
 			<div className="w-full">
-				<div
-					className="rounded-3xl bg-white px-4 py-6
+				<Fade cascade>
+					<div
+						className="rounded-3xl bg-white px-4 py-6
 			shadow-[rgba(29,_39,_87,_0.04)_0px_6px_10px] lg:px-2 xl:px-6 
 			"
-				>
-					<div className="image-icon-wrapper mx-auto h-[60px] w-[60px] md:h-[80px] md:w-[80px]">
-						<Image
-							src={props.thumbnailSrc}
-							alt={props.thumbnailAltText}
-							width={80}
-							height={80}
-							style={{
-								width: '100%',
-							}}
-						></Image>
+					>
+						<div className="image-icon-wrapper mx-auto h-[60px] w-[60px] md:h-[80px] md:w-[80px]">
+							<Image
+								src={props.thumbnailSrc}
+								alt={props.thumbnailAltText}
+								width={80}
+								height={80}
+								style={{
+									width: '100%',
+								}}
+							></Image>
+						</div>
+
+						<div className="text-center lg:pt-8">
+							<h3 className=" text-primary-black pb-4">{props.title}</h3>
+							<p className=" text-gray-shade-1 !font-thin ">{props.content}</p>
+						</div>
 					</div>
-					<div className="text-center lg:pt-8">
-						<h3 className=" text-primary-black pb-4">{props.title}</h3>
-						<p className=" text-gray-shade-1 !font-thin ">{props.content}</p>
-					</div>
-				</div>
+				</Fade>
 			</div>
 		</>
 	);
