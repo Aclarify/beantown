519c3bb2ade3e30f7d6bfe4a36b7b118d6bb7eea
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-03-14 18:10:42 +0530

    feat:about-us page animation added

--- beantown-website/components/templates/about-us/values/values-card.content.tsx
+++ beantown-website/components/templates/about-us/values/values-card.content.tsx
@@ -1,6 +1,7 @@
 import { GlobalContext } from '@contexts/global/global.context';
 import useWindowDimensions from '@lib/hooks/use-window-dimensions.hook';
 import { GlobalContextProps } from '@typing/common/interfaces/contexts.interface';
+import Animate from 'components/molecules/animate.molecule';
 import Image from 'next/image';
 import { AboutUsContentProps } from 'pages/about-us';
 import { useContext } from 'react';
@@ -17,37 +18,39 @@ const ValuesCards = () => {
 
 	return (
 		<>
-			<div
-				id="card-container"
-				className="text-primary-black flex flex-wrap justify-evenly gap-4 p-2"
-			>
-				{valuesCards?.map((cardInfo, index) => {
-					return (
-						cardInfo && (
-							<div
-								id="card-item"
-								className="flex w-[354px] items-center space-x-2 rounded-2xl bg-white p-3 shadow-sm"
-								key={index}
-							>
+			<Animate bottom cascade>
+				<div
+					id="card-container"
+					className="text-primary-black flex flex-wrap justify-evenly gap-4 p-2"
+				>
+					{valuesCards?.map((cardInfo, index) => {
+						return (
+							cardInfo && (
 								<div
+									id="card-item"
+									className="flex w-[354px] items-center space-x-2 rounded-2xl bg-white p-3 shadow-sm"
 									key={index}
-									className="bg-secondary-shade-3 flex w-1/3 items-center justify-center rounded-2xl py-8 px-5  "
 								>
-									<Image
-										alt={cardInfo?.iconImage?.asset?.altText || ''}
-										width={80}
-										height={80}
-										src={cardInfo?.iconImage?.asset?.url || ''}
-									/>
+									<div
+										key={index}
+										className="bg-secondary-shade-3 flex w-1/3 items-center justify-center rounded-2xl py-8 px-5  "
+									>
+										<Image
+											alt={cardInfo?.iconImage?.asset?.altText || ''}
+											width={80}
+											height={80}
+											src={cardInfo?.iconImage?.asset?.url || ''}
+										/>
+									</div>
+									<div className="w-2/3 pl-6">
+										<h5>{cardInfo?.text}</h5>
+									</div>
 								</div>
-								<div className="w-2/3 pl-6">
-									<h5>{cardInfo?.text}</h5>
-								</div>
-							</div>
-						)
-					);
-				})}
-			</div>
+							)
+						);
+					})}
+				</div>
+			</Animate>
 		</>
 	);
 };
