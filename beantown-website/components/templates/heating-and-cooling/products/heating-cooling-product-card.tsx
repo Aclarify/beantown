19a3b00ebd69100367ccef7eef3e09e15d6a7483
Ebalin1411 <110546356+Ebalin1411@users.noreply.github.com>
2023-03-21 15:13:45 +0530

    Update heating-cooling-product-card.tsx

--- beantown-website/components/templates/heating-and-cooling/products/heating-cooling-product-card.tsx
+++ beantown-website/components/templates/heating-and-cooling/products/heating-cooling-product-card.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
 import { Image as CMSImage } from '@typing/gql/graphql';
 import CMSImageWrapper from 'components/molecules/cms-image-wrapper.molecule';
+import Animate from 'components/molecules/animate.molecule';
+
+
 interface Props {
 	title: string;
 	thumbnailAltText: string;
@@ -11,20 +13,20 @@ interface Props {
 const ProductCard: React.FC<Props> = (props) => {
 	return (
 		<>
-			<div className="w-[281px] rounded-3xl bg-[#FCF7F7] p-2  lg:w-[480px] lg:p-4">
-				<div className=" flex h-[209px] justify-center rounded-3xl bg-white px-[30px] py-[20px] md:px-[50px]  md:py-[43px] lg:h-[360px] lg:max-h-full">
-					<CMSImageWrapper image={props.image} altText="A product image" />
-				</div>
-				<div className="flex flex-col items-start gap-2 px-2 py-4 pb-8 lg:px-4 lg:pt-8 ">
-					<h1 className="title-6 md:title-3 text-primary-black pb-2 lg:pb-4">
-						{props.title}
-					</h1>
+			<Animate>
+				<div className="w-[281px] rounded-3xl bg-[#FCF7F7] p-2  lg:w-[480px] lg:p-4">
+					<div className=" flex h-[209px] justify-center rounded-3xl bg-white px-[30px] py-[20px] md:px-[50px]  md:py-[43px] lg:h-[360px] lg:max-h-full">
+						<CMSImageWrapper image={props.image} altText="A product image" />
+					</div>
+					<div className="flex flex-col items-start gap-2 px-2 py-4 pb-8 lg:px-4 lg:pt-8 ">
+						<h3 className=" text-primary-black pb-2 lg:pb-4">{props.title}</h3>
 
-					<div className="para-4 md:para-3 text-gray-shade-1  text-left  ">
-						<p>{props.description}</p>
+						<div className=" text-gray-shade-1  text-left  ">
+							<p className="!font-thin">{props.description}</p>
+						</div>
 					</div>
 				</div>
-			</div>
+			</Animate>
 		</>
 	);
 };
