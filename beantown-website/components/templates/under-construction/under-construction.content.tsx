00222dfb8f5120a68113e37671956effe314adbf
Spencer Marx <spencer.s.marx@gmail.com>
2023-03-30 23:58:08 -0400

    chore: enhance under construction page

--- beantown-website/components/templates/under-construction/under-construction.content.tsx
+++ beantown-website/components/templates/under-construction/under-construction.content.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import Cog from '@heroicons/react/20/solid/CogIcon';
+
 // TODO : Fetch data from CMS
 const UnderConstructionContent = () => {
 	return (
@@ -8,7 +11,20 @@ const UnderConstructionContent = () => {
 			<div className="pt-48 pb-[89px]">
 				<div className=" flex flex-col justify-center text-center align-middle  ">
 					<div className=" text-primary-black px-[1em] pt-[2em] text-center md:mx-40 lg:px-[1em] lg:pt-0 xl:px-[2em] 2xl:px-[4em]">
+						<div className={clsx('py-12', 'w-full', 'flex', 'justify-center')}>
+							<Cog
+								className={clsx(
+									'h-40',
+									'w-40',
+									'animate-spin-slow',
+									'text-primary-shade-1'
+								)}
+							/>
+						</div>
 						<h1>{'This page is under construction'}</h1>
+						<p className={clsx('text-4xl', 'pt-6')}>
+							Check back soon for more content!
+						</p>
 					</div>
 				</div>
 			</div>
--- beantown-website/tailwind.config.js
+++ beantown-website/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			animation: {
+        'spin-slow': 'spin 3s linear infinite',
+      },
 			screens: {
 				xs: { max: '400px' },
 				'3xl': '2500px',
