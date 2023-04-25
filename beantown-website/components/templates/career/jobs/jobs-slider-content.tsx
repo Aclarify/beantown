d1854712b65aceead586d53791e61e755acb5c9c
Ebalin1411 <ebalinrosecse2006@gmail.com>
2023-04-25 20:19:05 +0530

    slider settings changed

--- beantown-website/components/templates/career/jobs/jobs-slider-content.tsx
+++ beantown-website/components/templates/career/jobs/jobs-slider-content.tsx
@@ -41,7 +41,7 @@ const JobsSliderContent = () => {
 		dots: false,
 		infinite: true,
 		speed: 1300,
-		slidesToShow: 1,
+		slidesToShow: 4,
 		slidesToScroll: 1,
 		arrows: false,
 		initialSlide: 0,
