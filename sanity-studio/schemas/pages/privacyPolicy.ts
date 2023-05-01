6a6fedbecd423ab18d5e21e72279162118ece17e
swineethamurthy <swineethamurthy@gmail.com>
2023-05-01 13:20:12 +0530

    feat : added schema for privacy policy

---
+++ sanity-studio/schemas/pages/privacyPolicy.ts
@@ -0,0 +1,64 @@
+import {defineField, defineType} from 'sanity'
+
+export default defineType({
+  name: 'privacyPolicy',
+  title: 'Privacy Policy',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'pageTitle',
+      title: 'HTML Page Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'metaDescription',
+      title: 'HTML Page Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'logoLight',
+      title: 'Logo Light',
+      type: 'reference',
+      to: [{type: 'imageIcon'}],
+    }),
+    defineField({
+      name: 'logoDark',
+      title: 'Logo Dark',
+      type: 'reference',
+      to: [{type: 'imageIcon'}],
+    }),
+    defineField({
+      name: 'heroTitle',
+      title: 'Hero Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'heroDescription',
+      title: 'Hero Description',
+      type: 'string',
+    }),
+    defineField({
+      name: 'heroImage',
+      title: 'Hero Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+    defineField({
+      name: 'sectionTitle',
+      title: 'Privacy Policy Section Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'privacyPolicyContent',
+      title: 'Privacy Policy Content',
+      type: 'portableText',
+    }),
+  ],
+})
