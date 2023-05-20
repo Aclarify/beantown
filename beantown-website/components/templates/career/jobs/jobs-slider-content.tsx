e87fbc52e70aad88dccf8b9dc7f6a797ffd7f571
Spencer Marx <spencer.s.marx@gmail.com>
2023-05-20 03:11:16 -0400

    chore: remove careers carousel debounce

--- beantown-website/components/templates/career/jobs/jobs-slider-content.tsx
+++ beantown-website/components/templates/career/jobs/jobs-slider-content.tsx
@@ -17,6 +17,87 @@ import { jobsContext } from '@contexts/jobs/Job-benefits-context';
 
 const animationSpeed = 500;
 
+const settings = {
+	dots: false,
+	infinite: true,
+	speed: animationSpeed,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	arrows: false,
+	initialSlide: 0,
+	focusOnSelect: true,
+	variableWidth: true,
+	useTransform: false,
+	swipe: false,
+	responsive: [
+		{
+			breakpoint: SCREEN_BREAKPOINTS.XXL,
+			settings: {
+				slidesToShow: 3.5,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: 1972,
+			settings: {
+				slidesToShow: 2.7,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: 1576,
+			settings: {
+				slidesToShow: 2.2,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: SCREEN_BREAKPOINTS.LG,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: 1040,
+			settings: {
+				slidesToShow: 1.8,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 1.8,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: SCREEN_BREAKPOINTS.SM,
+			settings: {
+				slidesToShow: 1.5,
+				slidesToScroll: 1,
+				variableWidth: true,
+			},
+		},
+		{
+			breakpoint: SCREEN_BREAKPOINTS.XS,
+			settings: {
+				slidesToShow: 1.1,
+				slidesToScroll: 1,
+				variableWidth: true,
+				ltr: true,
+			},
+		},
+	],
+};
+
 const JobsSliderContent = () => {
 	const slider = React.useRef<Slider | null>(null);
 	const { pageContent } =
@@ -79,88 +160,7 @@ const JobsSliderContent = () => {
 			}, animationSpeed);
 		}
 	};
-	const debouncedNext = debounce(next, 50);
-	const debouncedPrevious = debounce(previous, 50);
-	const settings = {
-		dots: false,
-		infinite: true,
-		speed: animationSpeed,
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		arrows: false,
-		initialSlide: 0,
-		focusOnSelect: true,
-		variableWidth: true,
-		useTransform: false,
-		swipe: false,
-		responsive: [
-			{
-				breakpoint: SCREEN_BREAKPOINTS.XXL,
-				settings: {
-					slidesToShow: 3.5,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: 1972,
-				settings: {
-					slidesToShow: 2.7,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: 1576,
-				settings: {
-					slidesToShow: 2.2,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: SCREEN_BREAKPOINTS.LG,
-				settings: {
-					slidesToShow: 2,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: 1040,
-				settings: {
-					slidesToShow: 1.8,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: 1024,
-				settings: {
-					slidesToShow: 1.8,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: SCREEN_BREAKPOINTS.SM,
-				settings: {
-					slidesToShow: 1.5,
-					slidesToScroll: 1,
-					variableWidth: true,
-				},
-			},
-			{
-				breakpoint: SCREEN_BREAKPOINTS.XS,
-				settings: {
-					slidesToShow: 1.1,
-					slidesToScroll: 1,
-					variableWidth: true,
-					ltr: true,
-				},
-			},
-		],
-	};
+
 	return (
 		<>
 			<div className=" w-full pl-5 pt-4 pb-8 md:pl-8 2xl:pl-[120px] ">
@@ -216,18 +216,17 @@ const JobsSliderContent = () => {
 						);
 					})}
 				</Slider>
-
 				<div className="relative mt-[76px] flex justify-between   p-2 pr-4 md:pr-24 ">
 					<button
 						aria-label={'left-arrow'}
-						onClick={debouncedPrevious}
+						onClick={previous}
 						className=" text-primary-shade-1 bg-secondary-shade-3 flex  h-12 w-20 justify-center rounded-full  px-6  py-4 md:px-12 "
 					>
 						<FontAwesomeIcon icon={faArrowLeft} />
 					</button>
 					<button
 						aria-label={'right-arrow'}
-						onClick={debouncedNext}
+						onClick={next}
 						className="bg-primary-shade-1 text-secondary-shade-3 flex h-12 w-20 justify-center rounded-full px-6  py-4  md:px-12 "
 					>
 						<FontAwesomeIcon icon={faArrowRight} />
