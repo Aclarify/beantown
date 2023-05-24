9e85af7fac09633ff0137af607d65874a4cfd53a
Spencer Marx <spencer.s.marx@gmail.com>
2023-05-24 17:41:38 -0400

    feat: add jest testing

---
+++ beantown-website/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	roots: ['<rootDir>'],
+	testMatch: [
+		'**/__tests__/**/*.spec.[jt]s?(x)',
+		'**/?(*.)+(spec|test).[tj]s?(x)',
+	],
+	testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+	setupFilesAfterEnv: ['<rootDir>/.jest/setup.jest.ts'],
+	transform: {
+		'^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+	},
+	moduleNameMapper: {
+		'\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+	},
+};
