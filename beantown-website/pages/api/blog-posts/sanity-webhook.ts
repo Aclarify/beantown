f14a458d9ab66432668f84a2866b34e882df53a2
Spencer Marx <spencer.s.marx@gmail.com>
2023-05-24 22:19:54 -0400

    fix: update blog post-processing

--- beantown-website/pages/api/blog-posts/sanity-webhook.ts
+++ beantown-website/pages/api/blog-posts/sanity-webhook.ts
@@ -1,79 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createAlgoliaRecord } from '@lib/api/blog/createAlgoliaRecord.handler';
 import { NextApiRequest, NextApiResponse } from 'next';
-import sanityClient from 'lib/clients/sanity/sanity.client';
-import indexer from 'sanity-algolia';
-import { algoliaAdminClient } from 'lib/clients/algolia/algolia.client';
-import { config } from 'lib/config';
-import { SIGNATURE_HEADER_NAME, isValidSignature } from '@sanity/webhook';
-
-export const BLOG_POSTS_PROJECTION = /* groq */ `{
-  	_type,
-    _rev,
-    _id,
-    "objectID": _id,
-    blogTitle,  
-    blogImage {
-      asset->{_id, _key, url, altText},
-    },
-    blogSlug {
-      current
-    },
-	blogContent[@._type != 'image'] {
-      "children": children [] {
-        text
-      }[0..3]
-    }[0..3],
-    blogTags [] -> {
-      _id, name, category
-    },
-    publishedAt,
-}`;
-
-const handlePost = async (req: NextApiRequest, res: NextApiResponse) => {
-	try {
-		// SecretKey-Signature security to prevent others from hitting this API
-		const signature = req.headers[SIGNATURE_HEADER_NAME] as string;
-		// Read the body into a string
-		const body = JSON.stringify(req.body);
-		if (!isValidSignature(body, signature, config.sanityWebhookSecret)) {
-			return res.status(401).json({ success: false });
-		}
-
-		const index = algoliaAdminClient.initIndex(
-			config.algoliaBlogSearchIndexName
-		);
-		const sanityAlgolia = indexer(
-			{
-				blogPosts: {
-					index,
-					projection: BLOG_POSTS_PROJECTION,
-				},
-			},
-			(document: any) => {
-				const publishedAt = new Date().toISOString();
-				const publishedAtTimestamp = Math.floor(Date.now() / 1000);
-				const updatedDoc = { ...document, publishedAt, publishedAtTimestamp };
-				return updatedDoc;
-			}
-		);
-
-		console.time('Syncing documents algolia to index:');
-		await sanityAlgolia.webhookSync(sanityClient, req.body);
-		console.timeEnd('Syncing documents algolia to index:');
-
-		res.status(200).json({ success: true });
-	} catch (error) {
-		console.error(error);
-		res.status(500).json({ success: false });
-	}
-};
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	const { method } = req;
 
 	switch (method) {
 		case 'POST':
-			await handlePost(req, res);
+			await createAlgoliaRecord(req, res);
 			break;
 		default:
 			res.setHeader('Allow', ['POST']);
