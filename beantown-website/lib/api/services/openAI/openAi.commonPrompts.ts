e1b8b617dea27ebe6c82b48ecac40e3f58dd82a0
Spencer Marx <spencer.s.marx@gmail.com>
2023-05-24 21:42:32 -0400

    chore: update prompt for blog summary

--- beantown-website/lib/api/services/openAI/openAi.commonPrompts.ts
+++ beantown-website/lib/api/services/openAI/openAi.commonPrompts.ts
@@ -1,1 +1,1 @@
-export const SUMMARIZE_BLOG = `Summarize the following blog post into a 100 word summary : {text}`;
+export const SUMMARIZE_BLOG = `Summarize the following blog post into a 100 word summary: {text}`;
