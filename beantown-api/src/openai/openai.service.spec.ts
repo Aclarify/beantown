fbc1c4ac37a816951c2deb89c7d529f2382e3d9a
Spencer Marx <spencer.s.marx@gmail.com>
2023-05-26 16:18:10 -0400

    fix: fix openai service and unit/int tests

--- beantown-api/src/openai/openai.service.spec.ts
+++ beantown-api/src/openai/openai.service.spec.ts
@@ -1,17 +1,17 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { OpenAIService, TranslatorOptions } from './openai.service';
-import { LLMChain, OpenAI } from 'langchain';
+import { OpenAIService } from './openai.service';
+import { OpenAIApi } from 'openai';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { openAIConfig } from '@beantown/config';
 
-const stubCall = jest.fn().mockResolvedValue({ text: '1' });
+const stubCall = jest.fn().mockResolvedValue({
+  data: {
+    choices: [{ text: '1' }],
+  },
+});
 
-class StubLLMChain {
-  call = stubCall;
-}
-
-class StubOpenAI {
-  constructor() {
-    return {};
-  }
+class StubOpenAIApi {
+  createCompletion = stubCall;
 }
 
 describe('OpenaiService', () => {
@@ -19,54 +19,43 @@ describe('OpenaiService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [ConfigModule.forRoot({ load: [openAIConfig] })],
       providers: [OpenAIService],
     }).compile();
 
-    service = module.get<OpenAIService>(OpenAIService);
+    service = await module.resolve<OpenAIService>(OpenAIService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  describe('createTranslator', () => {
-    const translatorOptions: TranslatorOptions = {
-      promptTemplate: 'Write me a haiku about: {text}',
-    };
-    it('should return a translator', () => {
-      const translator = service.createTranslator(
-        translatorOptions,
-        StubLLMChain as unknown as typeof LLMChain,
-        StubOpenAI as typeof OpenAI,
-      );
-      expect(translator).toBeDefined();
-      expect(typeof translator).toBe('function');
-    });
+  describe('summarizeBlog', () => {
+    it('should call openai.createCompletion', async () => {
+      const blogPostContent = 'blog post content';
 
-    it('should return a translator that calls a LLMChain chain', async () => {
-      const translator = service.createTranslator(
-        translatorOptions,
-        StubLLMChain as unknown as typeof LLMChain,
-        StubOpenAI as typeof OpenAI,
+      const result = await service.summarizeBlog(
+        blogPostContent,
+        StubOpenAIApi as unknown as typeof OpenAIApi,
       );
-
-      const result = await translator('flowers');
 
       expect(result).toBe('1');
+      expect(stubCall).toHaveBeenCalledWith({
+        model: 'text-davinci-003',
+        prompt:
+          'Summarize the following blog post into a 100 word summary: blog post content',
+      });
     });
-    /**
-     * The following integration test should be used sparingly
-     * as making requests to OpenAI is expensive.
-     */
-    // it('should call OpenAI with prompt and receive a response', async () => {
-    // 	const translator = createTranslator(translatorOptions);
+    it('should call openai.createCompletion', async () => {
+      const blogPostContent = 'This is blog post content';
 
-    // 	const result = await translator('flowers');
+      const result = await service.summarizeBlog(blogPostContent);
 
-    // 	expect(result).toBeDefined();
-    // 	expect(typeof result).toBe('string');
-    // }, 10000);
+      expect(result).toBeDefined();
+      expect(typeof result).toBe('string');
+    }, 10000);
   });
+
   describe('parseResponse', () => {
     it('should return a string', () => {
       const response = '\n\n\n\nThis  is\na	response';
--- beantown-api/src/openai/openai.service.ts
+++ beantown-api/src/openai/openai.service.ts
@@ -1,108 +1,55 @@
-import { OpenAIConfig, OpenAIOptions } from '@beantown/config';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { LLMChain, OpenAI, PromptTemplate } from 'langchain';
-import { LLMChainInput } from 'langchain/chains';
-import { OpenAIBaseInput } from 'langchain/dist/types/openai-types';
+import { OpenAIApi, Configuration } from 'openai';
 import { SUMMARIZE_BLOG } from './prompts/summarizeBlog.prompt';
-
-// Define the options for the translation function.
-export type TranslatorOptions = {
-  promptTemplate: string;
-  modelOptions?: Partial<OpenAIBaseInput>;
-  chainOptions?: Partial<LLMChainInput<string>>;
-};
-
-type PromptTemplateOptions = {
-  template: string;
-  inputVariables: string[];
-};
-
-// Define the type of the translation function.
-type Translator = (text: string) => Promise<string>;
+import { OpenAIOptions } from '@beantown/config';
+import { TOKEN } from './prompts/prompt.token';
 
 @Injectable()
 export class OpenAIService {
-  private readonly openAIApiKey: string;
   private readonly openAIOptions: OpenAIOptions;
+  private readonly configuration: Configuration;
 
   constructor(private configService: ConfigService) {
-    this.openAIApiKey =
-      this.configService.get<OpenAIConfig['openAIConfig']>(
-        'openAIConfig',
-      ).openAiApiKey;
-    console.log('OpenAI Key', this.openAIApiKey);
-
-    this.openAIOptions =
-      this.configService.get<OpenAIConfig['openAIOptions']>('openAIOptions');
+    const openAIApiKey = this.configService.get<string>(
+      'openAIConfig.openAiApiKey',
+    );
+    this.configuration = new Configuration({
+      apiKey: openAIApiKey,
+    });
+    this.openAIOptions = this.configService.get<OpenAIOptions>('openAIOptions');
   }
 
-  createTranslator(
-    options: TranslatorOptions,
-    _LLMChain = LLMChain,
-    _OpenAI = OpenAI,
-  ): Translator {
-    console.log(this.openAIOptions);
-    // Create the language model.
-    const model = new _OpenAI({
-      openAIApiKey: this.openAIApiKey,
-      modelName: this.openAIOptions.model,
-      ...options.modelOptions,
+  async makeRequest(prompt: string, data: string, _OpenAIApi = OpenAIApi) {
+    const openai = new _OpenAIApi(this.configuration);
+
+    const preppedPrompt = prompt.replace(TOKEN, data);
+
+    const response = await openai.createCompletion({
+      model: this.openAIOptions.model,
+      prompt: preppedPrompt,
     });
 
-    // Create the prompt template.
-    const promptOptions: PromptTemplateOptions = {
-      template: options.promptTemplate,
-      inputVariables: ['text'],
-    };
-
-    const prompt = new PromptTemplate(promptOptions);
-
-    // Create the chain.
-    const chainOptions: LLMChainInput<string> = {
-      llm: model,
-      prompt: prompt,
-      ...options.chainOptions,
-    };
-
-    const chain = new _LLMChain(chainOptions);
-
-    // This is the translation function.
-    const translate: Translator = async (text: string) => {
-      // Use the chain to generate a response.
-      const result = await chain.call({ text });
-
-      if (!result) {
-        throw new Error('No result was returned from OpenAI');
-      }
-
-      if (!result.text) {
-        throw new Error('No text was returned from OpenAI');
-      }
-
-      // The result is assumed to be a string.
-      return this.parseResponse(result.text as string);
-    };
-
-    // Return the translation function.
-    return translate;
+    return this.parseResponse(response.data.choices[0].text);
   }
 
-  parseResponse = (response: string): string =>
-    response
-      // Remove all multiple new lines
-      .replace(/\n\n+/g, '\n')
-      // Replace all single new lines with a space
-      .replace(/\n/g, ' ')
-      // Replace multiple spaces with single spaces
-      .replace(/\s\s+/g, ' ')
-      // Replace all tabs with single spaces
-      .replace(/\t/g, ' ')
-      // Remove leading and trailing spaces
-      .trim();
+  parseResponse(response: string): string {
+    return (
+      response
+        // Remove all multiple new lines
+        .replace(/\n\n+/g, '\n')
+        // Replace all single new lines with a space
+        .replace(/\n/g, ' ')
+        // Replace multiple spaces with single spaces
+        .replace(/\s\s+/g, ' ')
+        // Replace all tabs with single spaces
+        .replace(/\t/g, ' ')
+        // Remove leading and trailing spaces
+        .trim()
+    );
+  }
 
-  summarizeBlog = async (blogPostContent: string) =>
-    this.createTranslator({
-      promptTemplate: SUMMARIZE_BLOG,
-    })(blogPostContent);
+  async summarizeBlog(blogPostContent: string, _OpenAIApi = OpenAIApi) {
+    return this.makeRequest(SUMMARIZE_BLOG, blogPostContent, _OpenAIApi);
+  }
 }
